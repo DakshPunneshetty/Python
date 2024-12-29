d94eb37537eeafd9dc78beae9395b16174b0d00c
DakshPunneshetty <punneshettydaksh@gmail.com>
2024-12-29 22:42:11 +0530

    Introduction to Python

---
+++ 1.py
@@ -0,0 +1,15 @@
+# HOMEWORK 
+hi = "       Python is awesome"
+print(hi.upper())
+print(hi.lower())
+print(hi.strip())
+print(hi.replace("Python is awesome", "____Python  is awesome____"))
+
+
+hello = "HelloWorld"
+print(len(hello))
+
+
+
+
+print("Hello \nworld \nthis is backslash: /")
